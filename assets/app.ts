eba522711931b55f5a901ac11e1ababac5230d36
Indika Aruna <indika@localhost.localdomain>
2025-08-24 23:41:30 +0530

    Configure webpack

---
+++ assets/app.ts
@@ -0,0 +1,9 @@
+import "./styles/app.scss"; // Bootstrap SCSS imported
+import "bootstrap"; // Bootstrap JS
+
+// Example: programmatically show a modal
+// const myModalEl = document.getElementById("myModal");
+// if (myModalEl) {
+//     const myModal = new bootstrap.Modal(myModalEl);
+//     myModal.show(); // opens the modal
+// }
