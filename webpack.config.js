2d1a8b0b71520c5c60efd3392370868dfdd7d51e
indikaaruna8 <ins8plus@gmail.com>
2025-11-28 14:11:50 +0000

    WIP: before updating Symfony recipes

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.ts')
-    //.addEntry('main', './assets/main.ts')
+    .addEntry('app', './assets/app.js')
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
@@ -57,10 +57,10 @@ Encore
     })
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+    //.enableSassLoader()
 
     // uncomment if you use TypeScript
-    .enableTypeScriptLoader()
+    //.enableTypeScriptLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
